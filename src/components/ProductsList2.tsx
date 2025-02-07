94ee53ba2422fa0512eeff30d31c5b11e4714181
Kulsoom Arif <kulsoomumer969@gmail.com>
2025-02-07 08:59:43 -0800

    changes

--- src/components/ProductsList2.tsx
+++ src/components/ProductsList2.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image';
 import { sanityFetch } from "@/sanity/lib/fetch";
 import { eight } from "@/sanity/lib/queries";
 import { urlFor } from "@/sanity/lib/image";
-import Swal from 'sweetalert2';
-import { addtoCart } from '@/app/actions/actions';
 
 type Product = {
   _id: string;
@@ -29,16 +27,7 @@ type Product = {
 
 export default async function ProductsList2() {
   const products: Product[] = await sanityFetch({ query: eight });
-const handleAddtoCart = (e: React.MouseEvent, product: Product) => {
-    e.preventDefault();
-    Swal.fire({
-      position: "top-right",
-      icon:"success",
-      title:`${product.title} added to cart`,
-      showConfirmButton:false,
-      timer:1000
-    })
-    addtoCart (product);
+
   return (
     <div>
       <section className="bg-gray-100 py-16">
@@ -64,6 +53,9 @@ const handleAddtoCart = (e: React.MouseEvent, product: Product) => {
 )}
   <h2 className="text-xl font-bold text-center">{product.title}</h2>
                   <p className="text-center">${product.price}</p>
+                  
+                  
+
                 </div>
               </div>
             ))}
@@ -72,14 +64,6 @@ const handleAddtoCart = (e: React.MouseEvent, product: Product) => {
       </section>
     </div>
   );
-  <button
-       className="bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md              
-       hover:shadow-lg hover:scale-110 transition-transform duration-200 ease-in-out mt-2 w-full"
-       onClick={(e) => handleAddtoCart(e, product)}
-                  >
-                    Add To Cart
-                  </button>
-
+  
 }
 
-}
